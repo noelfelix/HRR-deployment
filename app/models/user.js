1256c93c8cd1918105ad113bdbeef432cdbaf8bf
Noel Felix <nfelix83@gmail.com>
2016-02-11 12:09:57 -0800

    First attempt at MongoDB conversion

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,37 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+
+  var userSchema = mongoose.Schema({
+      username: String,
+      password: String
+  });
+
+  userSchema.methods.initialize = function() {
+    this.hashPassword();
+  };
+
+  userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
       callback(isMatch);
     });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
-});
+  };
 
-module.exports = User;
+  userSchema.methods.hashPassword = function(){
+    var cipher = Promise.promisify(bcrypt.hash);
+    return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+    });
+  };
+
+  var User = mongoose.model('User', userSchema);
+
+  module.exports = User;
+});
